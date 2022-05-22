48b0950bb1ab05c2973e60c6c5d576f7c74af421
AHera <louriens@gmail.com>
2022-05-22 18:45:22 +0300

    add random component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@
 import { css } from "@emotion/react";
 
 import notes from "./data/notes.json";
+import RandomID from "./components/RandomID";
 
 function App() {
-  console.log(notes);
-  return (
-    <div
-      css={css`
-        background: yellow;
-      `}
-    >
-      app with emotion
+  const blocks = notes.map(({ title, date, description }) => (
+    <div className="note" key={RandomID(5)}>
+      <div className="title">{title}</div>
+      <div className="date">{date}</div>
+      <div className="description">{description}</div>
     </div>
-  );
+  ));
+
+  return <div>{blocks}</div>;
+
+  // return (
+  //   <div
+  //     css={css`
+  //       background: yellow;
+  //     `}
+  //   ></div>
+  // );
 }
 
 export default App;
